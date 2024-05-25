45ab0d937155a25cc8e0d39f636fe711aeb076ba
Ammar Elkosi <ammarrelkosii@yahoo.com>
2024-05-25 22:32:14 +0300

    updates

--- js/cursor.js
+++ js/cursor.js
@@ -1,26 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var stickyDiv = document.getElementById("cursor");
-  var links = document.querySelectorAll("a");
+  var innerCircle = document.getElementById("cursor");
+  var outerCircle = document.getElementById("outerCircle");
+
+  var links = document.querySelectorAll(".nav-item");
+  var caseStudyLinks = document.querySelectorAll(".case-study-card");
+
   // Function to update the position of the div based on mouse movement
   function moveDiv(event) {
     // Get the mouse coordinates
     var mouseX = event.clientX;
     var mouseY = event.clientY;
 
+    innerCircle.style.top = `${mouseY - innerCircle.offsetHeight / 2}px`;
+    innerCircle.style.left = `${mouseX - innerCircle.offsetWidth / 2}px`;
     // Set the position of the div
-    stickyDiv.style.left = mouseX - stickyDiv.offsetWidth / 2 + "px";
-    stickyDiv.style.top = mouseY - stickyDiv.offsetHeight / 2 + "px";
+    setTimeout(() => {
+      outerCircle.style.top = `${mouseY - outerCircle.offsetHeight / 2}px`;
+      outerCircle.style.left = `${mouseX - outerCircle.offsetWidth / 2}px`;
+    }, 50);
   }
 
   // Add event listener for mouse movement
   document.addEventListener("mousemove", moveDiv);
   links.forEach(function (link) {
     link.addEventListener("mouseenter", function (event) {
-      stickyDiv.classList.add("cursor-hovered");
+      outerCircle.classList.add("cursor-hovered");
+      caseStudyLinks.classList.add("outer-circle-removed");
     });
 
     link.addEventListener("mouseleave", function (event) {
-      stickyDiv.classList.remove("cursor-hovered");
+      outerCircle.classList.remove("cursor-hovered");
+      caseStudyLinks.classList.add("outer-circle-removed");
+    });
+  });
+  caseStudyLinks.forEach(function (link) {
+    link.addEventListener("mouseenter", function (event) {
+      outerCircle.classList.add("outer-circle-removed");
+    });
+
+    link.addEventListener("mouseleave", function (event) {
+      outerCircle.classList.remove("outer-circle-removed");
     });
   });
 });
